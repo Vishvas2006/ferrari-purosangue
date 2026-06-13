5f664804f504bc5362c33d6d275bd138a2a36f6a
Vishvas2006 <vishvas8814@gmail.com>
2026-06-13 17:28:30 +0530

    made changes to mobile css

--- script.js
+++ script.js
@@ -315,3 +315,28 @@ gsap.fromTo(
         }
     }
 );
+
+
+const menuBtn =
+document.querySelector(".menu-toggle");
+
+const navLinks =
+document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
+document
+.querySelectorAll(".nav-links a")
+.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
